a0f1699719a2b34c0c39650ba70142775b86855e
ahmedragab <ahmedragab20901@gmail.com>
2023-07-27 12:52:06 +0300

    👨🏻‍✈️ Repo License

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-console.log(`%c🎉Updated ARCatch`, 'color: #aaf; font-size: 1.2rem; font-weight: bold;');
+import { $catch } from "ar-catch";
 
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+const todo = async () => await $catch(url);
+
+console.log(todo());
+
+console.log(
+  `%c🎉Updated ARCatch`,
+  "color: #aaf; font-size: 1.2rem; font-weight: bold;"
+);
